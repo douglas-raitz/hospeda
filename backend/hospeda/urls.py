dcf05c48ac588ff9f79cb098375fbb9724077fed
Douglas Raitz <douglas_raitz@hotmail.com>
2026-08-26 19:28:20 -0300

    feat: Adc App de reservas na aplicação

--- backend/hospeda/urls.py
+++ backend/hospeda/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/', include('hospedes.urls')),
+    path('api/', include('reservas.urls')),
 ]
